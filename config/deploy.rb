dcdf5f715f57ac9b5447c92c8e5e683f37c42b75
Faxriddin <makhmadiyorovfakhriddin@gmail.com>
2022-08-12 13:35:48 +0500

    d

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@ lock "~> 3.17.1"
 
 set :application, "hello"
 set :repo_url, "https://github.com/FaxriddinMaxmadiyorov/hello.git"
+set :passenger_restart_with_touch, true
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
