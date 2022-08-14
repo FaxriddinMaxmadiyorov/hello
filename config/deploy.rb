91582374993139469cbf8e4f5d4fb052486adb25
Faxriddin <makhmadiyorovfakhriddin@gmail.com>
2022-08-14 15:30:19 +0500

    hh

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,3 +9,4 @@ set :deploy_to, "/home/deploy/#{fetch :application}"
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 set :keep_releases, 5
+set :passenger_restart_with_touch, true
