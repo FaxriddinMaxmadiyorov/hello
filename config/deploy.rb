952b5f669ce9859111670ae28d97377b02ac1592
Faxriddin <makhmadiyorovfakhriddin@gmail.com>
2022-08-14 16:42:05 +0500

    g

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,13 @@
 lock "~> 3.17.1"
 
 set :application, "hello"
-set :repo_url, "git@github.com:FaxriddinMaxmadiyorov/hello.git"
+set :repo_url, "https://github.com/FaxriddinMaxmadiyorov/hello.git"
 # set :passenger_restart_with_touch, true
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
+  'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 set :keep_releases, 5
 set :passenger_restart_with_touch, true
